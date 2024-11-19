936440f8b62a4bb87cbb2cb5b660ab316ef6b663
KenAlfizza <kenazalfizza@Outlook.com>
2024-11-18 21:49:28 -0500

    fix signup usecase and add test

--- src/test/java/use_case/SignupInteractorTest.java
+++ src/test/java/use_case/SignupInteractorTest.java
@@ -1,4 +1,54 @@
 package use_case;
 
-public class SignupInteractorTest {
+import data_access.FirestoreGroupDataAccessObject;
+import data_access.FirestoreUserDataAccessObject;
+import entity.*;
+import org.junit.jupiter.api.Test;
+import use_case.login.*;
+import use_case.signup.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SignupInteractorTest {
+
+    @Test
+    void successTest() {
+        SignupInputData inputData = new SignupInputData("Ken2", "ken@mail.com", "1234");
+        UserFactory userFactory = new CommonUserFactory();
+        GroupFactory groupFactory = new CommonGroupFactory();
+        ResponseFactory resposeFactory = new CommonResponseFactory();
+        MessageFactory messageFactory = new CommonMessageFactory();
+        RecommendationFactory recommendationFactory = new CommonRecommendationFactory();
+
+        FirestoreGroupDataAccessObject groupRepository = new FirestoreGroupDataAccessObject(
+                groupFactory,
+                resposeFactory,
+                messageFactory,
+                recommendationFactory);
+
+        SignupUserDataAccessInterface userRepository = new FirestoreUserDataAccessObject(userFactory, groupRepository);
+
+        SignupOutputBoundary successPresenter = new SignupOutputBoundary() {
+
+            @Override
+            public void prepareSuccessView(SignupOutputData outputData) {
+                System.out.println("Success");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                System.out.println(error);
+                fail("Use case failure is unexpected.");
+            }
+
+            @Override
+            public void switchToLoginView() {
+
+            }
+        };
+
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, successPresenter, userFactory);
+        interactor.execute(inputData);
+    }
 }
