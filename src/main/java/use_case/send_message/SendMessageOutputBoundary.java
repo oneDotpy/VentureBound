0e5b7f4f59532eae32008baf186bb74c9e3d125c
Leo Lei <lei.leo@mail.utoronto.ca>
2024-11-30 15:30:05 -0500

    removed unused classes in send message use case

--- src/main/java/use_case/send_message/SendMessageOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case.send_message;
-
-public interface SendMessageOutputBoundary {
-    public void prepareChatView(SendMessageOutputData sendMessageOutputData);
-
-    public void prepareFailView(String message);
-}
