d65d708e571c1d53e412fc1b460b54980bfdf612
oneDotpy <keenardhito@gmail.com>
2024-12-01 11:56:56 -0500

    added DAI for VacationBot

---
+++ src/main/java/use_case/vacation_bot/VacationBotDataAccesInterface.java
@@ -0,0 +1,12 @@
+package use_case.vacation_bot;
+
+import entity.Message;
+
+public interface VacationBotDataAccesInterface {
+    /**
+     * Returns the user with the given username.
+     * @param groupID the group ID of the group the message is sent to
+     * @param message the message sent
+     */
+    void updateMessage(String groupID, Message message);
+}
