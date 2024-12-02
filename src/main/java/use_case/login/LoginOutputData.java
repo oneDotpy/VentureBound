cb32071cc6f0f6365dfa88816e0fb5ce5f89bbf2
KenAlfizza <kenazalfizza@Outlook.com>
2024-12-01 20:14:14 -0500

    add javadoc to loginOutputData

--- src/main/java/use_case/login/LoginOutputData.java
+++ src/main/java/use_case/login/LoginOutputData.java
@@ -7,13 +7,21 @@ import entity.User;
 import java.util.List;
 
 /**
- * Output Data for the Login Use Case.
+ * Output data class for login.
+ * Contains the user object and the login status
  */
 public class LoginOutputData {
 
     private final User user;
     private final boolean useCaseFailed;
 
+    /**
+     * Constructor for new instance of LoginOutputData.
+     * @param username The username of the user logging in.
+     * @param email The email of the user logging in.
+     * @param group The group of the user logging in.
+     * @param useCaseFailed The state of the login.
+     */
     public LoginOutputData(String username, String email, Group group, boolean useCaseFailed) {
         if (group != null) {
             this.user = new CommonUser(username, "", email, group, group.getGroupID());
@@ -24,14 +32,18 @@ public class LoginOutputData {
         this.useCaseFailed = useCaseFailed;
     }
 
+    /**
+     * Gets the user object of the user logging in
+     * @return The user object obtained after logging in
+     */
     public User getUser() {
         return user;
     }
 
-    public boolean isUseCaseFailed() {
-        return useCaseFailed;
-    }
-
+    /**
+     * Gets the group object of the user after they log in
+     * @return The group object of the user logging in
+     */
     public Group getGroup() {
         return user.getGroup();
     }
