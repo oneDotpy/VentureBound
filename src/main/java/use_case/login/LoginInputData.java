4573a651ebacd767b9f20a47f318616f5ed6833b
KenAlfizza <kenazalfizza@Outlook.com>
2024-12-01 20:06:45 -0500

    add javadoc to loginInputData

--- src/main/java/use_case/login/LoginInputData.java
+++ src/main/java/use_case/login/LoginInputData.java
@@ -2,21 +2,35 @@ package use_case.login;
 
 /**
  * The Input Data for the Login Use Case.
+ * Contains information about the username and password
  */
 public class LoginInputData {
 
     private final String username;
     private final String password;
 
+    /**
+     * Constructor for an instance of LoginInputData
+     * @param username The username of the person logging in
+     * @param password The password of the person logging in
+     */
     public LoginInputData(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * Gets the username of the person logging in
+     * @return The username of the person logging in
+     */
     String getUsername() {
         return username;
     }
 
+    /**
+     * Gets the password of the user logging in
+     * @return The password of the user logging in
+     */
     String getPassword() {
         return password;
     }
