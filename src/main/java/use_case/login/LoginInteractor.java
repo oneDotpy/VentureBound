7e6e014cfcfb64fea2eb7193cb4b6674ad29899c
KenAlfizza <kenazalfizza@Outlook.com>
2024-12-01 19:49:58 -0500

    remove unused function

--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -43,16 +43,4 @@ public class LoginInteractor implements LoginInputBoundary {
             }
         }
     }
-
-    public void switchToChatView(LoginOutputData loginOutputData) {
-
-        loginPresenter.switchToChatView(loginOutputData);
-    }
-
-    public void switchToLoginView(LoginOutputData loginOutputData) {
-
-        loginPresenter.switchToWelcomeView(loginOutputData);
-
-    }
-
 }
